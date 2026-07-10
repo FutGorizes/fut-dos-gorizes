8933a03e7619c9e77212d9aff8e6f2a5691ab073
Bernardo Piccoli <bernardo@redrive.com.br>
2026-07-10 16:17:37 -0300

    remove: remove logs

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -1,69 +1,27 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-
 export async function createClient() {
+  const cookieStore = await cookies();
 
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
 
-console.log(
-"URL SUPABASE:",
-process.env.NEXT_PUBLIC_SUPABASE_URL
-);
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
 
-
-console.log(
-"KEY EXISTE:",
-!!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
-
-
-
-const cookieStore = await cookies();
-
-
-
-return createServerClient(
-
-process.env.NEXT_PUBLIC_SUPABASE_URL!,
-
-process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-
-{
-
-cookies: {
-
-getAll(){
-
-return cookieStore.getAll();
-
-},
-
-setAll(cookiesToSet){
-
-try{
-
-cookiesToSet.forEach(
-({name,value,options})=>{
-
-cookieStore.set(
-name,
-value,
-options
-);
-
-}
-
-);
-
-}catch{}
-
-}
-
-}
-
-}
-
-);
-
-
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) => {
+              cookieStore.set(name, value, options);
+            });
+          } catch {}
+        },
+      },
+    },
+  );
 }
